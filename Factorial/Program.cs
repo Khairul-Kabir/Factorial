fd94c886b9e64325441045078158b42407da799e
Khairul Kabir <sshakil.kabir60@gmail.com>
2021-04-17 13:03:02 +0600

    Update Program.cs

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,26 +1,14 @@
-﻿using System;
+using System;
 
 namespace Factorial
 {
     class Program
     {
-        static int[] arr;
-        static int j = 0;
         static void Main(string[] args)
         {
-            //Program program = new Program();
             Console.Write("Please input a number for get factorial: ");
             int getNumber = Convert.ToInt32(Console.ReadLine());
-            arr = new int[getNumber];
-            foo(getNumber);
-            //Console.WriteLine(res * res);
-            int result = getNumber;
-            
-            for (int i = 0; i < arr.Length-1; i++)
-            {
-                result *= arr[i];
-            }
-            Console.WriteLine("\nFactorial is: "+ result);
+            Console.WriteLine("\nFactorial is:"+ foo(getNumber));
         }
 
         public static dynamic foo(int n)
@@ -31,10 +19,7 @@ namespace Factorial
             }
             else
             {
-                Console.Write(n - 1 + ",");
-                arr[j] = (n - 1);
-                j++;
-                return foo(n - 1);
+                return n* foo(n - 1);
             }
         }
     }
